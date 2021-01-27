55a6115554a99b8526bfaefaa866d45fd1ee7ea8
LaRaHh <45185758+LaRaHh@users.noreply.github.com>
2021-01-27 16:56:29 -0300

    Correções no espaçamento do Header e demais partes do site, Menu mobile e footer corrigido

---
+++ apps/menuMobile.js
@@ -0,0 +1,24 @@
+const menuMobile = document.querySelectorAll("#menuToggle input");
+const menuMobileA = document.querySelectorAll("#menuToggle a");
+const input = document.getElementById("inputMenu")
+
+
+menuMobile.forEach(item => {
+    item.addEventListener("click", stopScroll)
+})
+
+function stopScroll(event){
+    if(input.checked){
+        document.body.style.overflow = "hidden"
+    } else{
+        document.body.style.overflow = "visible"
+    }
+}
+
+menuMobileA.forEach(i => {
+    i.addEventListener("click", closeMenu)
+})
+
+function closeMenu(i){
+    document.body.style.overflow = "visible"
+}
